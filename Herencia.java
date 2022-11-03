8091003a8673cd87323f231ca072589f54d7345b
Yanina Codega <106331416+ycode5@users.noreply.github.com>
2022-11-03 13:41:26 -0300

    Update Herencia.java

--- Herencia.java
+++ Herencia.java
@@ -2,83 +2,40 @@ public class Main {
     public static void main(String[] args) {
 
         Cliente cliente=new Cliente();
-        cliente.setNombre("Maria");
-        cliente.setEdad(29);
-        cliente.setTelefono(15376945);
-        cliente.setCredito(123);
-
-        System.out.println("Mi nombre es " + cliente.getNombre());
-        System.out.println("Tengo " + cliente.getEdad() + " años");
-        System.out.println("Mi telefono es " + cliente.getTelefono());
-        System.out.println("Mi credito es " + cliente.getCredito());
-
         Trabajador trabajador=new Trabajador();
-        trabajador.setNombre("Miguel");
-        trabajador.setEdad(26);
-        trabajador.setTelefono(15768743);
-        trabajador.setSalario(4000);
+        cliente.nombre ="Maria";
+        cliente.edad=29;
+        cliente.telefono=15376945;
+        cliente.credito= 123.03;
 
-        System.out.println("mi nombre es " + trabajador.getNombre());
-        System.out.println("Tengo " + trabajador.getEdad() + " años");
-        System.out.println("Mi telefono es " + trabajador.getTelefono());
-        System.out.println("Mi salario es de " + trabajador.getSalario());
+        System.out.println("Soy " + cliente.nombre + ", tengo " + cliente.edad + " años y mi telefono es " + cliente.telefono
+                + " y mi credito disponible es " + cliente.credito + " USD");
+
+        trabajador.nombre="Miguel";
+        trabajador.edad=26;
+        trabajador.telefono=15768743;
+        trabajador.salario= 4000.00;
+
+        System.out.println("Soy " + trabajador.nombre + ", tengo " + trabajador.edad + " años y mi telefono es " + trabajador.telefono
+                + " y mi salario es de " + trabajador.salario + "€");
 
         }
-
     }
 class Persona{
-    private String nombre;
-    private int edad;
-    private int telefono;
-
-    public void setNombre(String nombre) {
-        this.nombre=nombre;
-    }
-    public String getNombre(){
-        return this.nombre;
-    }
-    public void setEdad(Integer edad) {
-        this.edad=edad;
-    }
-    public  Integer getEdad(){
-
-        return this.edad;
-    }
-    public void setTelefono(Integer telefono){
-        this.telefono=telefono;
-    }
-    public Integer getTelefono(){
-
-        return this.telefono;
-    }
-
-
-
+     String nombre;
+     Integer edad;
+     Integer telefono;
 
 }
-
 class Cliente extends Persona{
-    private Integer credito;
-
-    public void setCredito(Integer credito ){
-        this.credito=credito;
-    }
-    public Integer getCredito(){
-        return this.credito;
-    }
-
+     Double credito;
 
 }
 
 class Trabajador extends Persona{
-    private Integer salario;
+     Double salario;
 
-    public void setSalario(Integer salario){
-        this.salario=salario;
-    }
-    public Integer getSalario(){
-        return this.salario;
-    }
 }
+
 
 
